365682ed5e6e809417c8d9cbff6c63c3d2cfc5af
salomaogit <63564287+salomaogit@users.noreply.github.com>
2020-05-07 15:52:32 -0400

    Commit atual

--- norte/view/menu.php
+++ norte/view/menu.php
@@ -1,8 +1,7 @@
 <nav class="navbar bg-dark">
     <ul class="nav">
         <li class="nav-item"><a href="?pag=inicio" class="nav-link">Início</a></li>
-        <li class="nav-item"><a href="?pag=quemsomos" class="nav-link">Quem Somos?</a></li>
-        <!-- <li class="nav-item"><a href="?pag=faleconosco" class="nav-link">Fale Conosco</a></li> -->
+        <li class="nav-item"><a href="?pag=quemsomos" class="nav-link">Empresa</a></li>
         <li class="nav-item"><a href="?pag=servicos" class="nav-link">Serviços</a></li>
     </ul>
     <form id="busca" class="btn-group" action="http://search.freefind.com/find.html" method="GET">
