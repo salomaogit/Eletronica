c851335585668d8ca64971cd476a66ae01d38545
salomaogit <63564287+salomaogit@users.noreply.github.com>
2020-05-07 18:52:28 -0400

    Update menu.php

--- norte/view/menu.php
+++ norte/view/menu.php
@@ -4,13 +4,13 @@
         <li class="nav-item"><a href="?pag=quemsomos" class="nav-link">Empresa</a></li>
         <li class="nav-item"><a href="?pag=servicos" class="nav-link">Serviços</a></li>
     </ul>
-    <form id="busca" class="btn-group" action="http://search.freefind.com/find.html" method="GET">
-        <input type=hidden name=id value=78127359>
-        <input type=hidden name=pid value=r>
-        <input type=hidden name=mode value=ALL>
-        <input type=hidden name=n value=0>
-        <input type=hidden name=lang value=pt>
-        <input type=text name=query size=20>
+    <form id="busca" class="btn-group" action="http://search.freefind.com/find.html" method="GET" target="_blank">
+        <input type="hidden" name="id" value="78127359">
+        <input type="hidden" name="pid" value="r">
+        <input type="hidden" name="mode" value="all">
+        <input type="hidden" name="n" value="0">
+        <input type="hidden" name="lang" value="pt">
+        <input type="text" name="query" size="20">
         <button class="btn btn-primary"><i class="fas fa-search"></i></button>
     </form>
 </nav>
