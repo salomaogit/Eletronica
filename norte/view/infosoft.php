db728667f2c67216663dfc5213077d32772046e6
salomaogit <63564287+salomaogit@users.noreply.github.com>
2020-05-06 21:28:45 -0400

    Commit modificação

--- norte/view/infosoft.php
+++ norte/view/infosoft.php
@@ -10,7 +10,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">Sistema Operacional</h5>
                     <p class="card-text">Instalação e recuperação de sistema operacional: Linux e Windows além de recuperação do sistema.</p>
-                    <a href="?pag=faleconosco&categ=infosoft&serv=sistema" class="btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>
         </div>
@@ -19,7 +19,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">Aplicativo</h5>
                     <p class="card-text">Instalação de aplicativos diversos como: office, adobe, cdBurner, CorelDRAW, PhotoShop e muitos outros.</p>
-                    <a href="?pag=faleconosco&categ=infosoft&serv=aplicativo" class="btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>        </div>
         <div class="col-sm-4 py-3">
@@ -27,7 +27,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">Backup / Recover</h5>
                     <p class="card-text">Backup de todo o sistema operacional com seus arquivos e unidades preservando todo o sistema. Recover para restaurar.</p>
-                    <a href="?pag=faleconosco&categ=infosoft&serv=backup" class="btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>
         </div>
@@ -38,7 +38,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">Malware</h5>
                     <p class="card-text">Limpeza e manutenção por sistema bootável ou pelo Sistema Operacional, ativação e execução de antmalwere´s.</p>
-                    <a href="?pag=faleconosco&categ=infosoft&serv=malware" class="btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>
         </div>
@@ -47,7 +47,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">Recorery</h5>
                     <p class="card-text">Recuperação de dados perdidos em unidades de discos diversas e recuperação de discos rígidos e lógicos.</p>
-                    <a href="?pag=faleconosco&categ=infosoft&serv=recovery" class="btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>
         </div>
@@ -56,7 +56,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">Outros</h5>
                     <p class="card-text">Se o seu serviço de SOFTWARE não condiz com nenhum dos serviços disponíveis nas opcões anteriores, utilize esta opção.</p>
-                    <a href="?pag=faleconosco&categ=infosoft&serv=outros" class="btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>
         </div>
