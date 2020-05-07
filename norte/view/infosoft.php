2596d0e2ba03174fc16ef233654bb6711fe960b5
salomaogit <63564287+salomaogit@users.noreply.github.com>
2020-05-07 00:30:52 -0400

    Commit infosoft.php

--- norte/view/infosoft.php
+++ norte/view/infosoft.php
@@ -10,7 +10,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">Sistema Operacional</h5>
                     <p class="card-text">Instalação e recuperação de sistema operacional: Linux e Windows além de recuperação do sistema.</p>
-                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20orçamento%20para%20a%20INSTALAÇÃO%20de%20um%20SISTEMA%20OPERACIONAL!%20Aguardo%20um%20retorno%20para%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>
         </div>
@@ -19,7 +19,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">Aplicativo</h5>
                     <p class="card-text">Instalação de aplicativos diversos como: office, adobe, cdBurner, CorelDRAW, PhotoShop e muitos outros.</p>
-                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20orçamento%20para%20a%20INSTALAÇÃO%20de%20um%20APLICATIVO!%20Aguardo%20um%20retorno%20para%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>        </div>
         <div class="col-sm-4 py-3">
@@ -27,7 +27,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">Backup / Recover</h5>
                     <p class="card-text">Backup de todo o sistema operacional com seus arquivos e unidades preservando todo o sistema. Recover para restaurar.</p>
-                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20orçamento%20para%20a%20RESTAURAÇÃO%20DE%20UM%20SISTEMA%20OU%20ARQUIVOS!%20Aguardo%20um%20retorno%20para%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>
         </div>
@@ -38,7 +38,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">Malware</h5>
                     <p class="card-text">Limpeza e manutenção por sistema bootável ou pelo Sistema Operacional, ativação e execução de antmalwere´s.</p>
-                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20orçamento%20para%20a%20DESINFECÇÃO%20LIMPEZA%20de%20um%20SISTEMA%20E%20ARQUIVOS!%20Aguardo%20um%20retorno%20para%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>
         </div>
@@ -47,7 +47,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">Recorery</h5>
                     <p class="card-text">Recuperação de dados perdidos em unidades de discos diversas e recuperação de discos rígidos e lógicos.</p>
-                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20orçamento%20para%20a%20RECUPERAÇÃO%20DE%20DADOS%20EM%20UNIDADES%20E/OU%20RECUPERAÇÃO%20DA%20UNIDADE%20DE%20DISCO!%20Aguardo%20um%20retorno%20para%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>
         </div>
@@ -56,7 +56,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">Outros</h5>
                     <p class="card-text">Se o seu serviço de SOFTWARE não condiz com nenhum dos serviços disponíveis nas opcões anteriores, utilize esta opção.</p>
-                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20orçamento%20para%20uma%20MANUTENÇÃO%20DE%20SOFTWARE!%20Aguardo%20um%20retorno%20para%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>
         </div>
--- norte/view/menu.php
+++ norte/view/menu.php
@@ -5,8 +5,11 @@
         <!-- <li class="nav-item"><a href="?pag=faleconosco" class="nav-link">Fale Conosco</a></li> -->
         <li class="nav-item"><a href="?pag=servicos" class="nav-link">Serviços</a></li>
     </ul>
-    <form id="busca" class="btn-group" action="#" method="post">
-        <input type="text">
+    <form id="busca" class="btn-group" action="http://www.google.com/search" method="GET">
+        <!-- <input type="text"> -->
+        <input type=hidden name=sitesearch/>
+        <input type=text name=q size=20>
+        <!-- <input type=submit value=Procurar> -->
         <button class="btn btn-primary"><i class="fas fa-search"></i></button>
     </form>
 </nav>
