e40578a7c51dd9402cbc061c3ee8c6378f76a137
salomaogit <63564287+salomaogit@users.noreply.github.com>
2020-05-06 22:36:47 -0400

    Commit css atual

--- norte/view/menusrv.php
+++ norte/view/menusrv.php
@@ -1,5 +1,5 @@
 <ol id="menu-srv">
     <li class="btn btn-primary"><a href="?pag=servicos&srv=eletronica" class="cor-w">Eletrônica</a></li>
-    <li class="btn btn-primary"><a href="?pag=servicos&srv=infohard" class="cor-w">Hardwere</a></li>
-    <li class="btn btn-primary"><a href="?pag=servicos&srv=infosoft" class="cor-w">Software</a></li>
+    <li class="btn btn-primary"><a href="?pag=servicos&srv=infohard" class="cor-w">Info-Hardwere</a></li>
+    <li class="btn btn-primary"><a href="?pag=servicos&srv=infosoft" class="cor-w">Info-Software</a></li>
 </ol>
