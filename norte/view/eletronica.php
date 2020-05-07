9c8f8eb29261ab45c39698b23dfdaf2ed8c5e822
salomaogit <63564287+salomaogit@users.noreply.github.com>
2020-05-06 22:56:44 -0400

    Update eletronica.php

--- norte/view/eletronica.php
+++ norte/view/eletronica.php
@@ -10,7 +10,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">Televisão</h5>
                     <p class="card-text">Fazemos sua manutenção e dispomos de telas, display, fonte, placas, controles, peças e acessórios em geral.</p>
-                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20orçamento%20para%20minha%20TELEVISÃO.%20Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20orçamento%20para%20minha%20TELEVISÃO.%20Aguardo%20um%20retorno%20para%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>
         </div>
@@ -19,7 +19,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">DVD / BlueRay</h5>
                     <p class="card-text">Temos peças para todas as marcas. Manutenção completa em seu leitor de discos com peças e assessórios em geral.</p>
-                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="card-lnk btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20orçamento%20para%20meu%20LEITOR%20DE%20DISCO!%20Aguardo%20um%20retorno%20para%20um%20atendimento%20da%20Norte%20Assistência!" class="card-lnk btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>        </div>
         <div class="col-sm-4 py-3">
@@ -27,7 +27,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">Data Show</h5>
                     <p class="card-text">Ajuste de lentes e limpeza. Manutenção na fonte, placa, gabinete e dispomos de todo equipamento para o diagnóstico.</p>
-                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20orçamento%20para%20um%20DATA%20SHOW!%20Aguardo%20um%20retorno%20para%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>
         </div>
@@ -38,7 +38,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">Aparelho de Som</h5>
                     <p class="card-text">Conserto de microsystem, aparelho de som e aparelhagem em geral. Dispomos de peças e assessorios para o seu som.</p>
-                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20orçamento%20para%20um%20APARELHO%20DE%20SOM!%20Aguardo%20um%20retorno%20para%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>
         </div>
@@ -47,7 +47,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">Dispositivo Móvel</h5>
                     <p class="card-text">Serviços como troca de tela, display e toutch, troca de placa, manutenção e limpeza, atém de serviços de recuperação.</p>
-                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20orçamento%20para%20um%20APARELHO%20CELULAR%20TABLET%20OU%20MOBILE!%20Aguardo%20um%20retorno%20para%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>
         </div>
@@ -56,7 +56,7 @@
                 <div class="card-body">
                     <h5 class="card-title  text-center">Outros</h5>
                     <p class="card-text">Se o seu serviço de ELETRÔNICA não condiz com os serviços disponíveis nas opcões anteriores, utilize esta opção.</p>
-                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
+                    <a href="http://wa.me/5592991388513?text=Solicito%20um%20orçamento%20para%20um%20EQUIPAMENTO%20ELETRÔNICO!%20Aguardo%20um%20retorno%20para%20um%20atendimento%20da%20Norte%20Assistência!" class="btn btn-primary">Solicitar Serviço</a>
                 </div>
             </div>
         </div>
