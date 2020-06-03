fa7bcb97a9e6305473e24c46d22d0fe4269280f0
salomaogit <63564287+salomaogit@users.noreply.github.com>
2020-06-03 17:33:30 -0400

    Update menuf.php

--- norte/view/menuf.php
+++ norte/view/menuf.php
@@ -3,34 +3,28 @@
     <div class="social">
         <div class="social-icons">
             <!-- <p class="share-title">Compartilhar:</p> -->
-            <a href="http://www.facebook.com/norteassistencia/" class="social-share-link" title="Norte Assistêcia no Facebook"
-                data-network-name="facebook">
+            <a href="http://www.facebook.com/norteassistencia/" class="social-share-link" title="Norte Assistêcia no Facebook" data-network-name="facebook" target="_blank">
                 <i class="fab fa-facebook-square text"></i>
             </a>
-            <a href="http://twitter.com/" class="social-share-link" title="Norte Assistêcia via Twiter"
-                data-network-name="twitter">
+            <a href="http://twitter.com/" class="social-share-link" title="Norte Assistêcia via Twiter" data-network-name="twitter" target="_blank">
                 <i class="fab fa-twitter-square text"></i>
             </a>
-            <!-- <a href="https://plus.google.com/" class="social-share-link" title="Norte Assistêcia via Google-Plus"
-                data-network-name="googleplus">
+            <!-- <a href="https://plus.google.com/" class="social-share-link" title="Norte Assistêcia via Google-Plus" data-network-name="googleplus" target="_blank">
                 <i class="fab fa-google-plus-square text"></i>
             </a> -->
-            <a href="https://www.youtube.com/" class="social-share-link" title="Norte Assistêcia via youtube"
-                data-network-name="youtube">
+            <a href="https://www.youtube.com/" class="social-share-link" title="Norte Assistêcia via youtube" data-network-name="youtube" target="_blank">
                 <i class="fab fa-youtube-square text"></i>
             </a>
-            <a href="https://bit.ly/2pXTwc6" class="social-share-link" title="Norte Assistêcia via WhatsApp" data-network-name="pinterest">
+            <a href="https://bit.ly/2pXTwc6" class="social-share-link" title="Norte Assistêcia via WhatsApp" data-network-name="pinterest" target="_blank">
                 <i class="fab fa-whatsapp-square text"></i>
             </a>
-            <a href="http://www.linkedin.com/" class="social-share-link" title="Norte Assistêcia via LinkedIn"
-                data-network-name="linkedin">
+            <a href="http://www.linkedin.com/" class="social-share-link" title="Norte Assistêcia via LinkedIn" data-network-name="linkedin" target="_blank">
                 <i class="fab fa-linkedin text"></i>
             </a>
-            <a href="mailto:norteassistenciatecnica@gmail.com?subject=Bom%20dia!%20Gostaria%20de%20informações!%20Estarei%20no%20aguardo%20de%20um%20retorno." class="social-share-link" title="Norte Assistêcia via Email" 
-            data-network-name="basic_email">
+            <a href="mailto:norteassistenciatecnica@gmail.com?subject=Bom%20dia!%20Gostaria%20de%20informações!%20Estarei%20no%20aguardo%20de%20um%20retorno." class="social-share-link" title="Norte Assistêcia via Email" data-network-name="basic_email" target="_blank">
                 <i class="fas fa-envelope-square text"></i>
             </a>
-            <!-- <a href="#" class="social-share-link" title="Compartilhar &quot; 3 maneiras de ter um botão do whatsapp em seu site.&quot; via Print" data-network-name="basic_print" data-share-title="3 maneiras de ter um botão do whatsapp em seu site." data-share-url="https://decisaodigital.com.br/blog/whataspp-no-seu-site/">
+            <!-- <a href="#" class="social-share-link" title="Compartilhar &quot; 3 maneiras de ter um botão do whatsapp em seu site.&quot; via Print" data-network-name="basic_print" data-share-title="3 maneiras de ter um botão do whatsapp em seu site." data-share-url="https://decisaodigital.com.br/blog/whataspp-no-seu-site/" target="_blank">
                 <i class="fas fa-print text"></i>
             </a> -->
         </div>
