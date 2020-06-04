0b1040305d447eef894aa2d00e408d7f092e9c3d
salomaogit <63564287+salomaogit@users.noreply.github.com>
2020-06-03 21:35:23 -0400

    Update menuf.php

--- norte/view/menuf.php
+++ norte/view/menuf.php
@@ -6,24 +6,24 @@
             <a href="http://www.facebook.com/norteassistencia/" class="social-share-link" title="Norte Assistêcia no Facebook" data-network-name="facebook" target="_blank">
                 <i class="fab fa-facebook-square text"></i>
             </a>
-            <a href="http://m.me/norteassistencia/" class="social-share-link" title="Norte Assistêcia no Messenger" data-network-name="messenger" target="_blank">
-                <i class="fab fa-facebook-messenger text"></i>
-            </a>
-            <!-- <a href="http://twitter.com/" class="social-share-link" title="Norte Assistêcia via Twiter" data-network-name="twitter" target="_blank">
-                <i class="fab fa-twitter-square text"></i>
-            </a> -->
             <!-- <a href="https://plus.google.com/" class="social-share-link" title="Norte Assistêcia via Google-Plus" data-network-name="googleplus" target="_blank">
                 <i class="fab fa-google-plus-square text"></i>
             </a> -->
             <a href="https://www.youtube.com/" class="social-share-link" title="Norte Assistêcia via youtube" data-network-name="youtube" target="_blank">
                 <i class="fab fa-youtube-square text"></i>
             </a>
+            <a href="http://m.me/norteassistencia/" class="social-share-link" title="Norte Assistêcia no Messenger" data-network-name="messenger" target="_blank">
+                <i class="fab fa-facebook-messenger text"></i>
+            </a>
             <a href="https://bit.ly/2pXTwc6" class="social-share-link" title="Norte Assistêcia via WhatsApp" data-network-name="pinterest" target="_blank">
                 <i class="fab fa-whatsapp-square text"></i>
             </a>
-            <a href="http://www.linkedin.com/" class="social-share-link" title="Norte Assistêcia via LinkedIn" data-network-name="linkedin" target="_blank">
+            <!-- <a href="http://www.linkedin.com/" class="social-share-link" title="Norte Assistêcia via LinkedIn" data-network-name="linkedin" target="_blank">
                 <i class="fab fa-linkedin text"></i>
-            </a>
+            </a> -->
+            <a href="http://twitter.com/" class="social-share-link" title="Norte Assistêcia via Twiter" data-network-name="twitter" target="_blank">
+                <i class="fab fa-twitter-square text"></i>
+            </a>            
             <a href="mailto:norteassistenciatecnica@gmail.com?subject=Bom%20dia!%20Gostaria%20de%20informações!%20Estarei%20no%20aguardo%20de%20um%20retorno." class="social-share-link" title="Norte Assistêcia via Email" data-network-name="basic_email" target="_blank">
                 <i class="fas fa-envelope-square text"></i>
             </a>
