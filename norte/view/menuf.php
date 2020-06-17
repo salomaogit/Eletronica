fbd9e1211464e7d437f3651465a41006ac804dbc
salomaogit <63564287+salomaogit@users.noreply.github.com>
2020-06-17 18:26:43 -0400

    Update menuf.php

--- norte/view/menuf.php
+++ norte/view/menuf.php
@@ -6,24 +6,24 @@
             <a href="http://m.me/norteassistencia/" class="social-share-link" title="Norte Assistêcia no Messenger" data-network-name="messenger" target="_blank">
                 <i class="fab fa-facebook-messenger text"></i>
             </a>
+            <a href="https://bit.ly/2pXTwc6" class="social-share-link" title="Norte Assistêcia via WhatsApp" data-network-name="pinterest" target="_blank">
+                <i class="fab fa-whatsapp-square text"></i>
+            </a>
             <!-- <a href="https://plus.google.com/" class="social-share-link" title="Norte Assistêcia via Google-Plus" data-network-name="googleplus" target="_blank">
                 <i class="fab fa-google-plus-square text"></i>
             </a> -->
-            <a href="https://www.youtube.com/" class="social-share-link" title="Norte Assistêcia via youtube" data-network-name="youtube" target="_blank">
+            <!-- <a href="https://www.youtube.com/" class="social-share-link" title="Norte Assistêcia via youtube" data-network-name="youtube" target="_blank">
                 <i class="fab fa-youtube-square text"></i>
-            </a>
+            </a> -->
             <a href="http://www.facebook.com/norteassistencia/" class="social-share-link" title="Norte Assistêcia no Facebook" data-network-name="facebook" target="_blank">
                 <i class="fab fa-facebook-square text"></i>
-            </a>
-            <a href="https://bit.ly/2pXTwc6" class="social-share-link" title="Norte Assistêcia via WhatsApp" data-network-name="pinterest" target="_blank">
-                <i class="fab fa-whatsapp-square text"></i>
             </a>
             <!-- <a href="http://www.linkedin.com/" class="social-share-link" title="Norte Assistêcia via LinkedIn" data-network-name="linkedin" target="_blank">
                 <i class="fab fa-linkedin text"></i>
             </a> -->
-            <a href="http://twitter.com/" class="social-share-link" title="Norte Assistêcia via Twiter" data-network-name="twitter" target="_blank">
+            <!-- <a href="http://twitter.com/" class="social-share-link" title="Norte Assistêcia via Twiter" data-network-name="twitter" target="_blank">
                 <i class="fab fa-twitter-square text"></i>
-            </a>            
+            </a> -->
             <a href="mailto:norteassistenciatecnica@gmail.com?subject=Bom%20dia!%20Gostaria%20de%20informações!%20Estarei%20no%20aguardo%20de%20um%20retorno." class="social-share-link" title="Norte Assistêcia via Email" data-network-name="basic_email" target="_blank">
                 <i class="fas fa-envelope-square text"></i>
             </a>
@@ -35,5 +35,5 @@
 </div> 
 <div class="text-center menuf">
     <!-- <h4><a href="mailto:norteassistenciatecnica@gmail.com">norteassistenciatecnica@gmail.com</a></h4> -->
-    Copyright &copy;<script>document.write(new Date().getFullYear());</script> Todos os direitos reservados | Contacte<i class="fa fa-heart-o" aria-hidden="true"></i> <a href="https://bit.ly/2PIXBJM" target="_blank">aqui</a> o criador do site.
+    Copyright &copy;<script>document.write(new Date().getFullYear());</script> Todos os direitos reservados | Clique<i class="fa fa-heart-o" aria-hidden="true"></i> <a href="https://bit.ly/2PIXBJM" target="_blank">aqui</a> e fale com o criador do site.
 </div>
